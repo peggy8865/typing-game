6cef5c25545ff4bc8a35ea0dac12f72b4a7775a1
peggy8865 <peggy8865@gmail.com>
2023-09-02 23:47:53 +0800

    fix: install eslint and fix typing.js file

--- public/typing.js
+++ public/typing.js
@@ -1,5 +1,5 @@
-const spareText = [`Artificial Intelligence (AI) has become an integral part of our lives, revolutionizing various industries and altering the way we perceive the world. With its advancements, AI applications are ubiquitous, ranging from smart assistants to autonomous vehicles. The benefits are undeniable, as AI enhances efficiency, accuracy, and convenience across sectors such as healthcare, finance, and manufacturing. However, concerns accompany the accelerated adoption of AI. Ethical considerations arise regarding data privacy, bias in algorithms, and potential job displacement. Acknowledging these challenges, researchers and policymakers strive to develop responsible AI frameworks. AI's transformative capabilities promise both incredible opportunities and substantial responsibilities. It is crucial to strike a balance between innovation and safeguards, ensuring AI remains a force for good in our rapidly evolving world. Embracing this technological marvel with vigilance and wisdom will lead us toward a brighter future where AI and humanity coexist harmoniously.`, 'apple bicycle butterfly chocolate dolphin eiffel tower forest friendship galaxy guitar happiness journey keyboard library marathon moonlight mountain parrot pineapple rainbow serendipity serenity strawberry summer sunshine telescope universe waterfall whisper winter salad hiked lake jagged shield defies lake shade flake dish gas fake deal skate hiked badge aisle flash gasp jail quiet rude tower rope equip rapid idea port diary water dart ear duty riot wheat proud quiet patio wire quiz box mix zinc save mice bean zoom cube oven vice sum maze bike voice foam nix tube vine scan maze analyze biology chemistry democracy equation fascinating geography historical imagination journalism knowledge literature mathematics negotiation opportunity psychology questionnaire revolutionary sustainable technology unique variable wildlife xenophobia youthful zeal abundant benevolent collaboration diligent ephemeral fortitude gravity harmonious integrity jubilant kinetic luminous metaphor nostalgia']
-const spareTextIndex = 0
+const spareText = ['Artificial Intelligence (AI) has become an integral part of our lives, revolutionizing various industries and altering the way we perceive the world. With its advancements, AI applications are ubiquitous, ranging from smart assistants to autonomous vehicles. The benefits are undeniable, as AI enhances efficiency, accuracy, and convenience across sectors such as healthcare, finance, and manufacturing. However, concerns accompany the accelerated adoption of AI. Ethical considerations arise regarding data privacy, bias in algorithms, and potential job displacement. Acknowledging these challenges, researchers and policymakers strive to develop responsible AI frameworks. AI\'s transformative capabilities promise both incredible opportunities and substantial responsibilities. It is crucial to strike a balance between innovation and safeguards, ensuring AI remains a force for good in our rapidly evolving world. Embracing this technological marvel with vigilance and wisdom will lead us toward a brighter future where AI and humanity coexist harmoniously.', 'apple bicycle butterfly chocolate dolphin eiffel tower forest friendship galaxy guitar happiness journey keyboard library marathon moonlight mountain parrot pineapple rainbow serendipity serenity strawberry summer sunshine telescope universe waterfall whisper winter salad hiked lake jagged shield defies lake shade flake dish gas fake deal skate hiked badge aisle flash gasp jail quiet rude tower rope equip rapid idea port diary water dart ear duty riot wheat proud quiet patio wire quiz box mix zinc save mice bean zoom cube oven vice sum maze bike voice foam nix tube vine scan maze analyze biology chemistry democracy equation fascinating geography historical imagination journalism knowledge literature mathematics negotiation opportunity psychology questionnaire revolutionary sustainable technology unique variable wildlife xenophobia youthful zeal abundant benevolent collaboration diligent ephemeral fortitude gravity harmonious integrity jubilant kinetic luminous metaphor nostalgia']
+let spareTextIndex = 0
 
 const words = document.getElementById('words')
 const timer = document.querySelector('.timer')
@@ -7,13 +7,14 @@ const resetBtn = document.getElementById('reset')
 const switchBtn = document.getElementById('switch')
 const WPM = document.querySelector('.WPM')
 const AR = document.querySelector('.AR')
+const comment = document.getElementById('comment')
 let id = 0
 let currentSpan
 let list
 let keydownTimes = 0
 let timerID
 
-function text2HTML(text) {
+function text2HTML (text) {
   let htmlText = ''
   if (Array.isArray(text)) {
     let spanId = 0
@@ -26,7 +27,6 @@ function text2HTML(text) {
       spanId++
     })
   } else {
-    if (typeof text === 'string') { }
     const typingArr = Array.from(text)
     typingArr.forEach((e, i) => {
       if (e === ' ') {
@@ -35,10 +35,10 @@ function text2HTML(text) {
         htmlText += `<span id="${i}" class="inwords">${e}</span>`
       }
     })
-  }  
+  }
   words.innerHTML = htmlText
 }
-function typeAChar(e) {
+function typeAChar (e) {
   if (id === 0 && !timerID) { // avoid setting the second timer when id === 0
     setTimer()
   }
@@ -82,14 +82,14 @@ function typeAChar(e) {
     }
   }
 }
-function calWPM() {
+function calWPM () {
   const secsUsed = 60 - timer.textContent
   return Math.round(keydownTimes / 6 * 60 / secsUsed) // a word + a space = 6 keys generally
 }
-function calAccuracyRate() {
+function calAccuracyRate () {
   return Math.round(id / keydownTimes * 100)
 }
-function giveComment(aWPM, aAR) {
+function giveComment (aWPM, aAR) {
   if (aWPM > 90 && aAR > 98) {
     return 'You are so fabulous!!! An expert!!!'
   } else if (aWPM >= 60 && aAR >= 95) {
@@ -102,7 +102,7 @@ function giveComment(aWPM, aAR) {
     return 'Good job!!'
   }
 }
-function giveResult() {
+function giveResult () {
   clearInterval(timerID)
   timerID = null
   const aWPM = calWPM()
@@ -110,22 +110,21 @@ function giveResult() {
   const aComment = giveComment(aWPM, aAR)
   WPM.textContent = aWPM
   AR.textContent = aAR
-  const comment = document.getElementById('comment')
   comment.textContent = aComment
 }
-function setTimer() {
+function setTimer () {
   timerID = setInterval(function () {
     timer.textContent -= 1
-    if (timer.textContent === "0") {
+    if (timer.textContent === '0') {
       giveResult()
     }
   }, 1000)
 }
-function cleanClass() {
+function cleanClass () {
   const nodesArr = document.querySelectorAll('.inwords')
   nodesArr.forEach(e => e.classList.remove('correct', 'wrong'))
 }
-function reset() {
+function reset () {
   if (timerID) {
     clearInterval(timerID)
     timerID = null
@@ -138,7 +137,7 @@ function reset() {
   id = 0
   keydownTimes = 0
 }
-function switchText() {
+function switchText () {
   axios.get('https://random-word-api.vercel.app/api?words=100')
     .then(response => {
       text2HTML(response.data)
@@ -159,4 +158,4 @@ switchBtn.addEventListener('click', () => {
   reset()
   switchText()
 })
-text2HTML(`Try typing the sentences here, then press a random key when finished. You can change the text with the "switch text" button below.`)
+text2HTML('Try typing the sentences here, then press a random key when finished. You can change the text with the "switch text" button below.')
